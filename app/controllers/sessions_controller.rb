56d6904c447c37067ca977a4067aea7337342c8a
miiku_shibata <miiku.shibata@crowdworks.co.jp>
2017-05-24 08:35:55 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
-    redirect_to toor_path
+    redirect_to root_path
   end
   
   private
